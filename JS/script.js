fc68bfd5c9c5c7bd46b42bad43c0b888e555fab6
Max <qmaksim1@gmail.com>
2020-10-01 12:57:38 +0300

    Добавлен скрипт и текст в хтмл

--- JS/script.js
+++ JS/script.js
@@ -9,10 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-      b = prompt('На сколько оцените его?', '');
-      c = prompt('Один из последних просмотренных фильмов?', '');
-      d = prompt('На сколько оцените его?', '');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+          b = prompt('На сколько оцените его?', '1-10');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } 
+    else {
+        console.log('error');
+        i--;
+    }
+}        
+
+if (personalMovieDB.count < 10 ) {
+    console.log('Смотрел мало фильмов. Деградант.');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Нормальный тип');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Киноман');
+} else {
+    console.log('Ошибочка');
+}
+
